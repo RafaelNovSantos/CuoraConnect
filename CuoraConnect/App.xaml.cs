dc74fa4df7133eea5be0ff0c789e1bc862252d4b
systelrafaelnovais <systelrafaelnovais@gmail.com>
2024-09-20 11:41:43 -0300

    Adicionar arquivos de projeto.

---
+++ CuoraConnect/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CuoraConnect
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
